02433b96cd81c90fcde6028d0d00c2cd0ade7638
sxxngwxn <sxxngw0n@gmail.com>
2024-08-08 20:53:25 +0900

    환율 계산 오류로 인해 취소

--- tracker.py
+++ tracker.py
@@ -1,21 +1,16 @@
 import requests
+import streamlit as st
 
-api_key = "ftdtZqgZMRnlxUbD0bL0hMOutS4Q21z7"
-
-def get_exchange_rate(date):
-    url = f"https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey={api_key}&searchdate=20180102&data=AP01"
+#선택한 암호화폐 가격
+def get_crypto_price(crypto_id):
+    url = f"https://api.coinpaprika.com/v1/tickers/{crypto_id}"
     response = requests.get(url)
-    return response
+    data = response.json()
+    return data['quotes']['USD']['price']
 
-# def get_crypto_price(crypto_id):
-#     url = f"https://api.coinpaprika.com/v1/tickers/{crypto_id}"
-#     response = requests.get(url)
-#     data = response.json()
-#     return data['quotes']['USD']['price']
+#선택한 코인
+crypto_id = 'btc-bitcoin'
 
-
-# crypto_id = 'btc-bitcoin'  # 비트코인 가격을 USD로 조회
-# coin_data = get_crypto_price(crypto_id)
-# print(coin_data)
-
-get_exchange_rate(20240807, "USD")
+#가격 가져오기(USD)
+coin_data = get_crypto_price(crypto_id)
+print(coin_data)
